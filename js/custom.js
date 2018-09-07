4f3529975012da5906bcda3ee7af61fae8269c72
Heshanibandaranayake <heshanibandaranayake@gmail.com>
2018-09-07 15:19:05 +0530

    create a user friendly validation icons for input fields in register.html

--- js/custom.js
+++ js/custom.js
@@ -24,13 +24,27 @@ function validatePassword(){
 
 	if (psw.length<8) {
 		document.getElementById("password").style.borderColor = "red";
-		document.getElementById("Email").style.backgroundImage ="url('images/false.png')";
+		document.getElementById("password").style.backgroundImage ="url('images/false.png')";
 
 	}
 	else{
 		document.getElementById("password").style.borderColor = "green"
-		document.getElementById("Email").style.backgroundImage ="url('images/true.png')";
+		document.getElementById("password").style.backgroundImage ="url('images/true.png')";
 	}
 }
 
+function validateConfirmPassword(){
+	var confirm = document.getElementById("Confirm").value;
+	var password = document.getElementById("password").value;
+
+	if (confirm!=password) {
+		document.getElementById("Confirm").style.borderColor = "red";
+		document.getElementById("Confirm").style.backgroundImage ="url('images/false.png')";
+	}
+	else{
+		document.getElementById("Confirm").style.borderColor = "green"
+		document.getElementById("Confirm").style.backgroundImage ="url('images/true.png')";
+	}
+
+}
 
