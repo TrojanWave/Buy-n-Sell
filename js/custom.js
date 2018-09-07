7b2f551cc7fdb3504378b5e15234d936484c32eb
Heshanibandaranayake <heshanibandaranayake@gmail.com>
2018-09-07 15:02:18 +0530

    set the validation of password

--- js/custom.js
+++ js/custom.js
@@ -7,15 +7,30 @@ function validateEmail(){
 
 	if(atPosition<1 || dotPosition<atPosition+2 || dotPosition+1>=email.length){
 		document.getElementById("Email").style.borderColor = "red";
-		document.getElementById("Email").style.backgroundImage ="url('images/wrong.jpg')";
+		document.getElementById("Email").style.backgroundImage ="url('images/false.png')";
+		
 
 
 	}
 	else{
 		document.getElementById("Email").style.borderColor = "green";
-
+		document.getElementById("Email").style.backgroundImage ="url('images/true.png')";
 	}
 	
+}
+
+function validatePassword(){
+	var psw = document.getElementById("password").value;
+
+	if (psw.length<8) {
+		document.getElementById("password").style.borderColor = "red";
+		document.getElementById("Email").style.backgroundImage ="url('images/false.png')";
+
+	}
+	else{
+		document.getElementById("password").style.borderColor = "green"
+		document.getElementById("Email").style.backgroundImage ="url('images/true.png')";
+	}
 }
 
 
