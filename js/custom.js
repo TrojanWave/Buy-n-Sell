990ee2ad15942a49ee8ae5a0fa5de58714fa5e2f
Heshanibandaranayake <heshanibandaranayake@gmail.com>
2018-09-07 14:04:29 +0530

    set the validation of email in register.html

--- js/custom.js
+++ js/custom.js
@@ -8,8 +8,7 @@ function validateEmail(){
 	if(atPosition<1 || dotPosition<atPosition+2 || dotPosition+1>=email.length){
 		document.getElementById("Email").style.borderColor = "red";
 		document.getElementById("Email").style.backgroundImage ="url('images/wrong.jpg')";
-		document.getElementById("Email").style.backgroundImageRepeat ="noRepeat";
-		document.getElementById("Email").style.backgroundImagePosition = "right";
+
 
 	}
 	else{
